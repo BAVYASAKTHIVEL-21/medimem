fe9468df0b4e51e63c4baf4ab51a63f65cf3af03
Bavya Sakthivel <bavyasakthivel21@gmail.com>
2026-07-04 12:29:29 +0530

    Medimem

--- medimem/backend/main.py
+++ medimem/backend/main.py
@@ -43,13 +43,13 @@ app = FastAPI(
     version="1.0.0",
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+allow_origins=[
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+
+    "https://medimem.vercel.app",
+    "https://medimem-m2lx8mewm-bavyasakthivel21-6805s-projects.vercel.app",
+]
 
 app.include_router(patients_router)
 app.include_router(upload_router)
